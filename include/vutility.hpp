b4b19da495f3bf2f2da658387889f6920a65f406
avinal <185067@nith.ac.in>
2020-08-21 18:51:18 +0530

    headers added

---
+++ include/vutility.hpp
@@ -0,0 +1,13 @@
+
+#include <vector>
+#include <string>
+#include<sstream>
+class vutility
+{
+private:
+    vutility() {}
+
+public:
+    static std::vector<std::string> split(std::string str, char delimit);
+    static std::string trim(std::string str);
+};
